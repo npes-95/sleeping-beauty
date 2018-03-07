5aba97061f874e591685aee42150aff5e86973ef
Erika Bondareva <erika.bondareva@glasgow.ac.uk>
2018-03-07 13:37:13 +0000

    app base code

---
+++ app/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Smart Alarm
+//
+//  Created by Erika Bondareva on 26/02/2018.
+//  Copyright © 2018 Sleeping Beauty. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ app/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Smart Alarm
+//
+//  Created by Erika Bondareva on 26/02/2018.
+//  Copyright © 2018 Sleeping Beauty. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
